3bab893920aa6e6c381db2e15b882a4777ebccd3
Bearhost <adedoyin.adetunji@miva.edu.ng>
2025-03-23 00:59:04 +0100

    added the function to convert ASCII code to character

--- ascii_converter.py
+++ ascii_converter.py
@@ -11,4 +11,24 @@ def ascii_converter(text):
         if len(user_input) == 1:
             char = user_input
             ascii_code = ord(char)
-            print(f'The ASCII code of "{char}" is {ascii_code}.')
+            if ascii_code <= 255:
+                print(f'The ASCII code of "{char}" is {ascii_code}. (Hex: {hex(ascii_code)})')
+            else:
+                print(f'Error: "{char}" is not an ASCII character.')
+                
+    # ASCII code to character
+        else:
+            try:
+                ascii_code = int(user_input)
+                if 0 <= ascii_code <= 255:
+                    char = chr(ascii_code)
+                    print(f'The character of ASCII code {ascii_code} is "{char}".')
+                else:
+                    print(f'Error: {ascii_code} is not an ASCII code.')
+            except ValueError:
+                print('Error: Invalid input.')
+                
+                
+                
+if __name__ == '__main__':
+    ascii_converter('text')
